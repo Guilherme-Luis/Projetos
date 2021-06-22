ff897c8a0464b265f63f12664bb58d07134e7022
Guilherme-Luis <guilherme_silva_luis@hotmail.com>
2021-06-21 21:42:53 -0300

    first commit

---
+++ aatstech.com.br/script_java.js
@@ -0,0 +1,36 @@
+
+function Padrao(){
+    window.location.assign("compras/padrao.php");
+}
+function Pro(){
+    window.location.assign("compras/pro.php");
+}
+function Ultra(){
+    window.location.assign("compras/ultra.php");
+}
+function credito(){
+    alert("Desculpa o transtorno, estamos melhorando a sua expêriencia!");
+}
+function debito(){
+    alert("Desculpa o transtorno, estamos melhorando a sua expêriencia!");
+}
+function boleto(){
+    alert("Desculpa o transtorno, estamos melhorando a sua expêriencia!");
+}
+function cadastroUsuario(){
+    window.location.assign("cadastroUsuario.php");
+}
+function verUsuarios(){
+    window.location.assign("verUsuarios.php");
+}
+function excliurUsuario(){
+    window.location.assign("");
+}
+function vendasEfetudas(){
+    alert("Desculpa o transtorno, estamos melhorando a sua expêriencia!");
+}
+function produtos(){
+    alert("Desculpa o transtorno, estamos melhorando a sua expêriencia!");
+}
+
+//<script src="C:\Users\Guilherme\Desktop\Técnico T.I\Projeto Integrador\Projeto Senac\script_java.js"></script>
